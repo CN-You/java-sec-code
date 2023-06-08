b9a99052ccf4d4468b398cf72f99cb65fe81e270
CN-You <99465056+CN-You@users.noreply.github.com>
2023-06-08 13:46:50 +0800

    Update HttpService.java

--- src/main/java/org/joychou/service/HttpService.java
+++ src/main/java/org/joychou/service/HttpService.java
@@ -9,3 +9,27 @@ public interface HttpService {
 
     String RequestHttpBanRedirects(String url, HttpHeaders headers);
 }
+
+public class FastJsonRequest extends RestRequestor<JSONObject> {
+
+    public FastJsonRequest(String url) {
+	    this(url, RequestMethod.GET);
+    }
+
+    public FastJsonRequest(String url, RequestMethod requestMethod) {
+	    super(url, requestMethod);
+    }
+
+    @Override
+    public JSONObject parseResponse(Headers header, byte[] body) throws Throwable {
+	    String result = StringRequest.parseResponseString(headers, body);
+	    return JSON.getTimestamp(result);
+    }
+}
+
+public interface HttpService {
+
+    String RequestHttp(String url, HttpHeaders headers);
+
+    String RequestHttpBanRedirects(String url, HttpHeaders headers);
+}
